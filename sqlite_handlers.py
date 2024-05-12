1eda4efcf92610d530428e819c49ac63acf83174
Daniel Nolte <knowtea@comcast.net>
2024-05-11 22:12:46 -0400

    added sql query error handler v1

--- sqlite_handlers.py
+++ sqlite_handlers.py
@@ -4,22 +4,25 @@ import sqlite3
 # -------------------- DB METHODS -------------------- #
 def execute_query(query_string: str, convert_to_dict=True):
     print('QUERY:   ' + query_string)
-    with sqlite3.connect("CookBookDatabase.db") as con:
-        db_cursor = con.cursor()
-        result = db_cursor.execute(query_string).fetchall()
-        if convert_to_dict:
-            # convert the resulting List to a Dict and return it to the caller
-            columns = [desc[0] for desc in db_cursor.description]
-            results = []
-            for row in result:
-                summary = dict(zip(columns, row))
-                results.append(summary)
-            print(results)
-            return results
-        else:
-            # if not requested in Dict form, return as a List, which is readily combobox-compatible if defined correctly
-            print(result)
-            return result
+    try:
+        with sqlite3.connect("CookBookDatabase.db") as con:
+            db_cursor = con.cursor()
+            result = db_cursor.execute(query_string).fetchall()
+            if convert_to_dict:
+                # convert the resulting List to a Dict and return it to the caller
+                columns = [desc[0] for desc in db_cursor.description]
+                results = []
+                for row in result:
+                    summary = dict(zip(columns, row))
+                    results.append(summary)
+                print(results)
+                return results
+            else:
+                # if not requested in Dict form, return as a List, which is combobox-compatible if defined correctly
+                print(result)
+                return result
+    except Exception as e:
+        return e.with_traceback()
 
 
 def execute_update_script(script_string: str):
